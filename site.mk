ae73dd39af9bb4bd85b6fe2109017be07d9af9e1
Jan-Philipp Litza <janphilipp@litza.de>
2014-01-11 10:57:56 +0100

    initial version with basic config (should work)

---
+++ site.mk
@@ -0,0 +1,13 @@
+GLUON_SITE_PACKAGES := \
+	gluon-config-mode \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-mesh-batman-adv \
+	gluon-mesh-vpn-fastd \
+	gluon-autoupdater \
+	iputils-ping6 \
+	iwinfo \
+	iptables \
+	kmod-ipt-nathelper \
+	firewall \
+	haveged
