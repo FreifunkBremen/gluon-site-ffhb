48249e1fd00194549d893d0e55937a5fa478816f
Jan-Philipp Litza <janphilipp@litza.de>
2014-03-24 10:47:02 +0100

    sorted gluon-* packages by name, added gluon-autoupdater-changer

--- site.mk
+++ site.mk
@@ -1,16 +1,17 @@
 GLUON_SITE_PACKAGES := \
+	gluon-alfred \
+	gluon-autoupdater \
+	gluon-autoupdater-switcher \
 	gluon-config-mode \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
-	gluon-autoupdater \
-	gluon-alfred \
+	gluon-next-node \
+	gluon-status-page \
 	iputils-ping6 \
 	iwinfo \
 	iptables \
 	kmod-ipt-nathelper \
 	firewall \
-	haveged \
-	gluon-status-page \
-	gluon-next-node
+	haveged
