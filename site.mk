bd9181f4f02ac0ccad0c06e85fcef7373c8070bb
Jan-Philipp Litza <janphilipp@litza.de>
2014-06-23 01:29:35 +0200

    Update version number to be higher than testing3

--- site.mk
+++ site.mk
@@ -16,7 +16,7 @@ GLUON_SITE_PACKAGES := \
 	firewall \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.3.101-nightly$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.3.102-nightly$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
