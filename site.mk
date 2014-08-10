596b61770eb3157764a7e0ef0fc2fb767fa651b2
Jan-Philipp Litza <janphilipp@litza.de>
2014-08-10 16:34:29 +0200

    Increased nightly version to new testing

--- site.mk
+++ site.mk
@@ -23,7 +23,7 @@ GLUON_SITE_PACKAGES := \
 	firewall \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.3.102-nightly$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.5~testing4-nightly$(shell date '+%Y%m%d')
 DEFAULT_GLUON_PRIORITY := 0
 
 # Allow overriding the release number from the command line
