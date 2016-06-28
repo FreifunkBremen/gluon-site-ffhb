32a79b12736b9d0b14bc71852caaeaf813d19cae
Martin Geno <geno+dev@fireorbit.de>
2016-06-28 11:54:59 +0200

    first try

--- site.mk
+++ site.mk
@@ -1,33 +1,16 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-respondd \
-	gluon-autoupdater \
-	gluon-config-mode-core \
-	gluon-config-mode-hostname \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
+	gluon-respondd-wifisettings \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
-	gluon-mesh-vpn-fastd \
-	gluon-next-node \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-speedtest \
-	gluon-status-page \
-	gluon-wan-dnsmasq-static \
+	gluon-authorized-keys \
 	iputils-ping6 \
 	iwinfo \
 	iptables \
 	firewall \
 	haveged
 
+DEFAULT_GLUON_RELEASE := 2016-breminale1
 DEFAULT_GLUON_PRIORITY := 0
 
 # Allow overriding the release number and languages from the command line
