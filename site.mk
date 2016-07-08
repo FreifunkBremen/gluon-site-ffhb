a1080192ce4e92834b59fe311ff21318bf6a69f5
Julian Kornberger <jk+github@digineo.de>
2016-07-09 00:58:49 +0200

    Update packages

--- site.mk
+++ site.mk
@@ -1,14 +1,16 @@
 GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-respondd \
-	gluon-respondd-wifisettings \
 	gluon-respondd-airtime \
+	gluon-respondd-lldp \
+	gluon-respondd-wifisettings \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-authorized-keys \
 	iputils-ping6 \
 	iwinfo \
 	iptables \
 	firewall \
+	reghack \
 	haveged
 
 DEFAULT_GLUON_RELEASE := 2016-breminale1
