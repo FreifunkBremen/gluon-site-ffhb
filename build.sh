4979b6308ef76b85077fff1c714112188d3b9ef3
Simon Joda Stößer <me@simjo.st>
2016-11-09 17:36:19 +0100

    change build.sh to now autosign testing.manifest

--- build.sh
+++ build.sh
@@ -146,7 +146,7 @@ make manifest GLUON_BRANCH=testing GLUON_PRIORITY=0
 cd ..
 
 if [ -n "$KEYFILE" -a -r "$KEYFILE" ]; then
-  "${GLUON_DIR}/contrib/sign.sh" "$KEYFILE" "${GLUON_DIR}/output/images/sysupgrade/${GLUON_BRANCH}.manifest"
+  "${GLUON_DIR}/contrib/sign.sh" "$KEYFILE" "${GLUON_DIR}/output/images/sysupgrade/testing.manifest"
 fi
 
 rm "$statefile"
