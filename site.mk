aa4e3211698b76878ade524ed5513aad4ddb219f
Simon Joda Stößer <me@simjo.st>
2017-04-15 14:53:48 +0200

    site.mk: match the comment to gluons example

--- site.mk
+++ site.mk
@@ -31,7 +31,7 @@ GLUON_SITE_PACKAGES := \
 	firewall \
 	haveged
 
-# Allow overriding the these variables from the command line
+# Variables set with ?= can be overwritten from the command line
 GLUON_RELEASE ?= $(patsubst v%,%,$(shell git -C $(GLUON_SITEDIR) describe --tags --dirty=+))
 export GLUON_BRANCH ?= stable
 GLUON_PRIORITY ?= 0
