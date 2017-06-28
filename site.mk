29fe6dec11f8055162528463541c9b6a63b4b581
Martin Geno <geno+dev@fireorbit.de>
2017-06-28 09:51:29 +0200

    Breminale: cleanup with minimals

--- site.mk
+++ site.mk
@@ -1,34 +1,19 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
+	gluon-mesh-babel \
+	gluon-l3roamd \
 	gluon-respondd \
 	respondd-module-airtime \
-	gluon-autoupdater \
-	gluon-config-mode-core \
-	gluon-config-mode-hostname \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-segment-mld \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-mesh-vpn-fastd \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
-	gluon-mesh-vpn-fastd \
-	gluon-next-node \
-	gluon-radv-filterd \
+	respondd-module-wifisettings \
+	gluon-respondd-lldp \
+	gluon-client-bridge \
 	gluon-radvd \
-	gluon-setup-mode \
-	gluon-speedtest \
-	gluon-status-page \
+	gluon-authorized-keys \
+	ffhb-breminale \
 	iputils-ping6 \
 	iwinfo \
 	iptables \
 	firewall \
+	reghack \
 	haveged
 
 # Variables set with ?= can be overwritten from the command line
@@ -37,4 +22,4 @@ export GLUON_BRANCH ?= stable
 GLUON_PRIORITY ?= 7
 GLUON_LANGS ?= en de
 GLUON_REGION ?= eu
-GLUON_ATH10K_MESH ?= ibss
+GLUON_ATH10K_MESH ?= 11s
