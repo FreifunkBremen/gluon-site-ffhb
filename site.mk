30f1d35949b82dbb80f4cc262d339882da1eeecc
Martin/Geno <geno+dev@fireorbit.de>
2018-08-22 02:30:01 +0200

    add gluon-ebtables-source-filter

--- site.mk
+++ site.mk
@@ -11,6 +11,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-source-filter \
 	gluon-web-admin \
 	gluon-web-autoupdater \
 	gluon-web-mesh-vpn-fastd \
